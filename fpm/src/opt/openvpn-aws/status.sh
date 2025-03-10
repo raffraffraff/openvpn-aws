37fda3d1ad086e8af2648d0c7436a315325f3abb
Paul Rafferty <raffraffraff@gmail.com>
2025-03-10 13:20:07 +0000

    Improving tray.sh / status.sh

--- fpm/src/opt/openvpn-aws/status.sh
+++ fpm/src/opt/openvpn-aws/status.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 RUN_DIR=/var/run/user/${UID}/openvpn-aws
 
-STATUS=$(cat ${RUN_DIR}/status)
-CURRENT_CONNECTION=$(cat ${RUN_DIR}/current_connection.txt)
+if [ -f "${RUN_DIR}/status" ]; then
+  STATUS=$(cat ${RUN_DIR}/status)
+else
+  STATUS="unknown status"
+fi
+if [ -f "${RUN_DIR}/current_connection.txt" ]; then
+  CURRENT_CONNECTION=$(cat ${RUN_DIR}/current_connection.txt)
+else
+  CURRENT_CONNECTION="Unknown connection"
+fi
 
-notify-send --app-name openvpn-aws --expire-time 3000 "marina-internal-euw3" "Status: connected"
+notify-send --app-name openvpn-aws --expire-time 3000 "${CURRENT_CONNECTION}" "Status: ${STATUS}"
--- fpm/src/opt/openvpn-aws/tray.sh
+++ fpm/src/opt/openvpn-aws/tray.sh
@@ -1,31 +1,45 @@
 #!/bin/bash
 RUN_DIR=/var/run/user/${UID}/openvpn-aws
+mkdir -p ${RUN_DIR}
 
-fileage() {
-  echo $(( $( date +%s) - $(stat -c %Y -- "${RUN_DIR}/openvpn.pid") ))
+interval() {
+  if [ -f "${RUN_DIR}/openvpn.pid" ]; then
+    fileage=$(( $( date +%s) - $(stat -c %Y -- "${RUN_DIR}/openvpn.pid") ))
+    if [ $(( ${fileage} % 300 )) -gt 295 ]; then
+      return 0
+    fi
+  fi
+  return 1
+}
+
+running() {
+  if [ -f ${RUN_DIR}/openvpn.pid ]; then
+    pgrep -F ${RUN_DIR}/openvpn.pid >/dev/null
+    return $?
+  fi
+  return 1
 }
 
 watcher() {
   while [ 1 ]; do
-    if [ -f ${RUN_DIR}/openvpn.pid ]; then
-      pgrep -F ${RUN_DIR}/openvpn.pid
-      echo "OpenVPN pid file found, running status $RUNNING"
-      if [ $(( $(fileage) % 300 )) -gt 295 ]; then
+    if running; then
+      echo "connected" > ${RUN_DIR}/status
+      if interval; then
         CURRENT_CONNECTION=$(cat $RUN_DIR/current_connection.txt)
-	notify-send --app-name openvpn-aws --expire-time 3000 "openvpn-aws" "REMINDER: You are still connected to ${CURRENT_CONNECTION}!" --transient
+        notify-send --app-name openvpn-aws --expire-time 3000 "openvpn-aws" "REMINDER: You are still connected to ${CURRENT_CONNECTION}!" --transient
       fi
     else
-      echo "No OpenVPN pid found"
+      echo "disconnected" > ${RUN_DIR}/status
     fi
     sleep 5
   done
-} 
+}
 
-mkdir -p ${RUN_DIR}
+# Exit if an existing tray is running
 touch ${RUN_DIR}/tray.pid
 pkill --signal 0 -F ${RUN_DIR}/tray.pid
 if [ $? -eq 0 ]; then
-  echo "AWS ClientVPN running. Use tray icon to connect/disconnect or exit"
+  echo "AWS ClientVPN Manager is already running. Use tray icon to connect/disconnect or exit"
   exit
 else
   echo $$ > ${RUN_DIR}/tray.pid
