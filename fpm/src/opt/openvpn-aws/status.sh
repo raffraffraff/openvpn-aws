25cbc0d222b5c485236d0202762ee51efe02b50f
Paul Rafferty <raffraffraff@gmail.com>
2025-03-10 12:19:35 +0000

    Replacing the status.sh "yad" command with a "notify-send"

--- fpm/src/opt/openvpn-aws/status.sh
+++ fpm/src/opt/openvpn-aws/status.sh
@@ -4,9 +4,4 @@ RUN_DIR=/var/run/user/${UID}/openvpn-aws
 STATUS=$(cat ${RUN_DIR}/status)
 CURRENT_CONNECTION=$(cat ${RUN_DIR}/current_connection.txt)
 
-yad --button="${CURRENT_CONNECTION} ${STATUS:-unknown}:0" \
-      --mouse \
-      --skip-taskbar \
-      --undecorated \
-      --on-top \
-      --timeout 2
+notify-send --app-name openvpn-aws --expire-time 3000 "marina-internal-euw3" "Status: connected"
